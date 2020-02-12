0a83783cd6a53484de1eb318b7b54aa7bb321512
Dheeraj Chand <dheeraj.chand@gmail.com>
2020-02-12 11:49:36 -0600

    1. This should work

--- redistricting_queries.sql
+++ redistricting_queries.sql
@@ -65,13 +65,13 @@ CREATE INDEX pln_cong_sdx
 DROP TABLE IF EXISTS plan_cong_all_int_rep;
 
 CREATE TABLE plan_cong_all_int_rep AS
-    (SELECT src.pid AS pid,
-            src.plan_id AS plan_id,
-            src.cong_id AS cong_id,
-            src.plan_name || ' - ' || src.cong_name AS composing_districts,
+    (SELECT src.pid                                                 AS pid,
+            src.plan_id                                             AS plan_id,
+            src.cong_id                                             AS cong_id,
+            src.plan_name || ' - ' || src.cong_name                 AS composing_districts,
             ST_AREA(src.intersection_geom) / ST_AREA(src.plan_geom) AS overlap_ratio_for_plan,
             ST_AREA(src.intersection_geom) / ST_AREA(src.cong_geom) AS overlap_ratio_for_congress,
-            src.intersection_geom   AS geom
+            src.intersection_geom                                   AS geom
      FROM plan_cong_all_int_src src);
 
 DROP INDEX IF EXISTS
@@ -79,9 +79,9 @@ DROP INDEX IF EXISTS
 
 CREATE INDEX IF NOT EXISTS
     pln_cng_all_int_sdx
-ON
-    plan_cong_all_int_rep
-    USING GIST(geom);
+    ON
+        plan_cong_all_int_rep
+            USING GIST (geom);
 
 -- Get all parts of the districts that aren't contained by the intersecting CD
 
@@ -96,37 +96,43 @@ CREATE TABLE plan_cong_noncntned_int_src AS (
            census.namelsad                                                   AS cong_name,
            plan.geom                                                         AS plan_geom,
            census.geom                                                       AS cong_geom,
-           ST_MULTI(ST_BUFFER(ST_INTERSECTION(plan.geom, census.geom), 0.0)) AS intersection_geom
+           ST_DIFFERENCE(plan.geom,ST_MULTI(ST_BUFFER(ST_INTERSECTION(plan.geom, census.geom), 0.0))) AS disjunction_geom
     FROM ca_five_districts AS plan
              INNER JOIN tl_2019_us_cd_116 AS census
                         ON (ST_INTERSECTS(plan.geom, census.geom))
     WHERE NOT ST_ISEMPTY(ST_BUFFER(ST_INTERSECTION(plan.geom, census.geom), 0.0)));
-
-CREATE INDEX pln_cong_noncntned_sdx
-    ON
-        plan_cong_noncntned_int_src
-            USING GIST (intersection_geom);
-
-DROP TABLE IF EXISTS plan_cong_noncntned_int_rep;
-
-CREATE TABLE plan_cong_noncntned_int_rep AS
-    (SELECT src.pid AS pid,
-            src.plan_id AS plan_id,
-            src.cong_id AS cong_id,
-            src.plan_name || ' - ' || src.cong_name AS composing_districts,
-            ST_AREA(src.intersection_geom) / ST_AREA(src.plan_geom) AS overlap_ratio_for_plan,
-            ST_AREA(src.intersection_geom) / ST_AREA(src.cong_geom) AS overlap_ratio_for_congress,
-            src.intersection_geom   AS geom
-     FROM plan_cong_noncntned_int_src src);
+);
 
 DROP INDEX IF EXISTS
     pln_cong_noncntned_rep_sdx;
 
+
+DROP TABLE IF EXISTS
+    plan_cong_noncntned_int_rep;
+
+CREATE TABLE plan_cong_noncntned_int_rep AS
+    (SELECT src.pid                                                 AS pid,
+            src.plan_id                                             AS plan_id,
+            src.cong_id                                             AS cong_id,
+            src.plan_name || ' - ' || src.cong_name                 AS composing_districts,
+            ST_AREA(src.disjunction_geom) / ST_AREA(src.plan_geom) AS overlap_ratio_for_plan,
+            ST_AREA(src.disjunction_geom) / ST_AREA(src.cong_geom) AS overlap_ratio_for_congress,
+            src.disjunction_geom                                   AS geom
+     FROM plan_cong_noncntned_int_src src);
+
 CREATE INDEX IF NOT EXISTS
     pln_cong_noncntned_rep_sdx
-ON
-    plan_cong_noncntned_int_rep
-    USING GIST(geom);
-
+    ON
+        plan_cong_noncntned_int_rep
+            USING GIST (geom);
 
 -- GET ALL TAB BLOCKS THAT THE INTERSECTION CONTAINS AND AREAS
+
+-- SCRATCH AREA
+
+SELECT COUNT(*) AS total_count,
+       COUNT(dsjnct.*) AS disjunct_count,
+       COUNT(cnjnct.*) AS conjunct_count
+       FROM plan_cong_noncntned_int_rep dsjnct, plan_cong_all_int_rep cnjnct
+WHERE dsjnct.composing_districts = cnjnct.composing_districts
+AND st_equals(dsjnct.geom, cnjnct.geom)
